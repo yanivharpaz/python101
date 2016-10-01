4fb3489a8749faa09cff444fa4eb58e1eaaf76c9
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2016-10-01 20:03:07 +0300

    Word game, version 0.01

--- python_words.py
+++ python_words.py
@@ -1,12 +1,27 @@
 
+def get_random_word():
+    return 'pizza'
 
 def play_game():
     print('Playing game')
+    word = get_random_word()
+    strikes = 0
+    max_strikes = 3
+
+    playing = True
+
+    while playing:
+        strikes += 1
+
+        if strikes >= max_strikes:
+            playing = False
+
 
 def main():
     print('Word game started.')
     play_game()
 
+    print('Game has ended, thank you for participating.')
 
 
 
