62137a22feccab0c3022c4c18880bf4fbb59193e
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2016-10-03 20:28:01 +0300

    main comment

--- concepts/oop.py
+++ concepts/oop.py
@@ -1,18 +1,41 @@
-
-class Person():
-
+class Classroom:
     def __init__(self):
-        print('started')
-        self.name = 'default'
+        self._people = []
 
-    def say_hello(self, param):
-        print("Hello!", param, self.name)
+    def add_person(self, person):
+        self._people.append(person)
+
+    def remove_person(self, person):
+        self._people.remove(person)
+
+    def greet(self):
+        for person in self._people:
+            person.say_hello()
+
+
+class Person:
+
+    def __init__(self, name):
+#        print('started')
+        self.name = name
+
+    def say_hello(self):
+        print("Hello!", self.name)
 
 
 def main():
-    p1 = Person()
-    p1.say_hello('hi')
 
+    p1 = Person('Yaniv')
+#    p1.say_hello('hi')
+    p1.name = 'another'
+#    p1.say_hello('hi')
+
+    room = Classroom()
+    room.add_person(Person('Yaniv'))
+    room.add_person(Person('Limor'))
+    room.add_person(Person('Chief'))
+    room.greet()
 
 if __name__ == "__main__":
     main()
+
