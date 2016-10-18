0c85d4dfe5c183a549a7e6cf6dfa32acdf041a31
yanivharpaz <yaniv.harpaz@gmail.com>
2016-10-18 07:39:00 +0300

    added limor02

--- limor02.py
+++ limor02.py
@@ -1,5 +1,7 @@
 import sys
 
+from math import log
+
 
 def main(argv):
     print("This is my program")
@@ -9,3 +11,34 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv)
+
+
+def convert(s):
+    try:
+        x=int(s)
+        print("convert success!", x)
+    except (ValueError,TypeError):
+        x = -1
+        print("convert failed!", x)
+    return x
+
+#convert(5)
+#convert([1,2,3])
+#convert("limor")
+
+
+def convert_error(a):
+    try:
+        return int(a)
+    except (ValueError,TypeError) as e:
+        print("convertion error:{}".format(str(e)),file=sys.stderr)
+        return -1
+
+convert_error("lim")
+
+
+def string_log(s):
+    v = convert(s)
+    return log(v)
+
+string_log("1")
