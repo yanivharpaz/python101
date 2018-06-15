f441689bfb0cc9f198764b8c976f801bdb32d268
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2018-06-15 12:53:41 +0300

    split to FileDocumentMap class

---
+++ FileDocumentMap.py
@@ -0,0 +1,50 @@
+import csv
+
+
+class FileDocumentMap(object):
+    """
+    Class to map the FileDocument
+    """
+
+    def __init__(self):
+        self._document_body = {'header': {
+                                            'top_line': 1,
+                                            'bottom_line': None
+                                         },
+                                'table': {
+                                             'top_line': None,
+                                             'bottom_line': None,
+                                             'data_pages': None
+                                           },
+                                'footer': {
+                                             'top_line': None,
+                                             'bottom_line': None
+                                           },
+                                }
+
+    def get_header_bottom_line(self):
+        return self._document_body['header']['bottom_line']
+
+    def set_header_bottom_line(self, line_number):
+        self._document_body['header']['bottom_line'] = line_number
+        self._document_body['table']['top_line'] = line_number + 1
+
+    def get_table_bottom_line(self):
+        return self._document_body['table']['bottom_line']
+
+    def set_table_bottom_line(self, line_number):
+        self._document_body['table']['bottom_line'] = line_number
+        self._document_body['footer']['top_line'] = line_number + 1
+
+    def set_table_data_pages(self, table_lines):
+        self._document_body['table']['data_pages'] = table_lines
+
+    def get_table_data_pages(self):
+        return self._document_body['table']['data_pages']
+
+    def persist(self, dest_path, file_name):
+        with open(dest_path + file_name, 'w') as csvfile:
+            for row in self.get_table_data_pages():
+                writer = csv.writer(csvfile, lineterminator='\n', quotechar='"', quoting=csv.QUOTE_ALL)
+                writer.writerow(row)
+
--- cl01.py
+++ cl01.py
@@ -1,47 +1,16 @@
+import  os.path
 import sys
+from FileDocumentMap import FileDocumentMap
 
-
-class FileDocumentMap(object):
-    """
-    Class to map the FileDocument
-    """
-    def __init__(self):
-        self._document_body = {'header': {
-                            'top_line': 1,
-                            'bottom_line': None
-                            },
-                          'table': {
-                            'top_line': None,
-                            'bottom_line': None
-                            },
-                          'footer': {
-                            'top_line': None,
-                            'bottom_line': None
-                            },
-                          }
-
-    def get_header_bottom_line(self):
-        return self._document_body['header']['bottom_line']
-
-    def set_header_bottom_line(self, line_number):
-        self._document_body['header']['bottom_line'] = line_number
-        self._document_body['table']['top_line'] = line_number + 1
-
-    def get_table_bottom_line(self):
-        return self._document_body['table']['bottom_line']
-
-    def set_table_bottom_line(self, line_number):
-        self._document_body['table']['bottom_line'] = line_number
-        self._document_body['footer']['top_line'] = line_number + 1
-
-    def persist(self, file_name):
-        # TODO: save the document body to a file
-        pass
-
+# ------------------------------------
+# updated: 15/06/2018
+# ------------------------------------
 
 # ----------------
 # general functions
 # ----------------
+
+
 def string_dump(input_string):
     result_list = [ord(current_char) for current_char in input_string]
     return result_list
@@ -50,12 +19,12 @@ def string_dump(input_string):
 def u200_strip(input_string):
     return input_string.replace('\u200e', ' ').strip()
 
-
 # ----------------
 # main
 # ----------------
-def main(file_path=r't:\tmp\cl01.txt'):
 
+
+def main(file_path= r't:\tmp\cl01.txt'):
     ROWS_LIMIT = 30000
     file_Document_map = FileDocumentMap()
 
@@ -63,79 +32,81 @@ def main(file_path=r't:\tmp\cl01.txt'):
 
     rows_type = {
                     "date": "‎:תאריך"
-                }
+                 }
 
-    print("Opening ", file_path)
+    print("Opening ",  file_path)
     with open(file_path, encoding="Windows-1255") as input_file:
-        # lines = input_file.read().strip('\u200e').split('\n')
         lines = input_file.read().split('\n')
-        print(len(lines))
+
+        # title
+        list_title_names = list()
+        line1 = u200_strip(lines[17]).split('|')
+        line2 = u200_strip(lines[18]).split('|')
+        line3 = u200_strip(lines[19]).split('|')
+        title = zip(line1, line2, line3)
+        for row in title:
+            list_title_names.append(''.join(row))
 
         line_counter = 0
-
         page_start = 0
         page_end = 0
+
         for line in lines[:ROWS_LIMIT]:
             line_counter += 1
-            # line_items = line.split('\u200e')
             # print(len(line_items), line_items)
-            print(line_counter, len(line), "]{}[".format(line))
+            # print(line_counter, len(line), "]{}[".format(line))
 
             # find header lines
-            if line.strip().startswith("* *"):
-                print('*** Header row ***')
+            # if line.strip().startswith("* *"):
+            #     print('*** Header row ***')
+
+            list_pages = list()
 
             # find page breaks
             if line.endswith(rows_type["date"]):
                 if file_Document_map.get_header_bottom_line() is None:
-                    print("setting header boundaries")
                     file_Document_map.set_header_bottom_line(line_counter)
 
-                print('*** Page Break ***')
+                # print('*** Page Break ***')
                 file_Document_map.set_table_bottom_line(line_counter)
                 page_start = line_counter
 
-            if line.endswith("-" * 10):
-                print('*** Line Separator ***')
+            # if line.endswith("-" *  10):
+                # print('*** Line Separator ***')
 
             if line.endswith("\f"):
-                print("End of table ", line_counter)
-
-                # add page borders to the pages list
+                # print("End of table ", line_counter)
                 page_end = line_counter
                 pages_list.append((page_start, page_end))
                 file_Document_map.set_table_bottom_line(line_counter)
 
-        print("end of header ", file_Document_map.get_header_bottom_line())
-        print("end of table ", file_Document_map.get_table_bottom_line())
+        # print("end of header ", file_Document_map.get_header_bottom_line())
+        # print("end of table ", file_Document_map.get_table_bottom_line())
 
-        print(pages_list)
+        # print(pages_list)
         table_lines = list()
-        _ = pages_list.pop(0)       # remove first page - it's the header
+        table_lines.append(list_title_names[1:])  # add title to list
+        _ = pages_list.pop(0)  # remove first page - it's the header
+
         for page in pages_list:
             start_line, end_line = page
-            print(start_line, end_line, end_line - start_line)
+            print("row_number_table_start: {} row_number_table_end: {} num_rows: {}".format(start_line, end_line, end_line - start_line))
             line_counter = start_line + 8   # skip 8 lines of table header
             while line_counter < end_line:
-                line_to_add = u200_strip(lines[line_counter]) + " " + u200_strip(lines[line_counter + 2])   # merge two data lines
-                table_lines.append(line_to_add)
+                line_to_add = str(u200_strip(lines[line_counter]) + " " + u200_strip(lines[line_counter + 2])).split('|')   # merge two data lines
+                line_to_add = [line.strip() for line in line_to_add]
+                table_lines.append(line_to_add[1:len(line_to_add) - 1])
                 line_counter += 4
-                print(line_to_add)
+                print(line_to_add[1:len(line_to_add) - 1])
 
-        # print("table lines:", table_lines)
-        #parse only table
-        # for row in lines[file_Document_map.get_header_bottom_line():file_Document_map.get_table_bottom_line()]:
-        #     if not (row.endswith("-" * 10) or row.endswith("\f") or len(u200_strip(row).split('|')) == 1):
-        #          list_lines = u200_strip(row).split('|')
-        #          print(list_lines[1:len(list_lines)-1])
+    file_Document_map.set_table_data_pages(table_lines)
 
+    file_name = os.path.splitext((os.path.basename(file_path)))[0] + '.csv'
+    #  TODO: add dest_path to argv
+    dest_path = "T:\\tmp\\"  #"c:\\"
 
-    # print(sep_counter)
-    print('\n\n\n')
-    # print(u200_strip(lines[20]))
-    # print(u200_strip(lines[21]))
-    # print(20, string_dump(u200_strip(lines[20])))
-    # print(21, string_dump(u200_strip(lines[21])))
+    # write data to csv file
+    file_Document_map.persist(dest_path, file_name)
 
 
 if __name__ == "__main__":
